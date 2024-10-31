d171f0e1965f266c2f75675d3270e4a3dedc3ab3
Ivan Frolkov <ifrol2001@mail.ru>
2024-10-31 20:28:02 +0300

    Fix db schema

--- src/main/java/shaif/jobs/GenericCallCapableJobHandler.java
+++ src/main/java/shaif/jobs/GenericCallCapableJobHandler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
@@ -17,6 +18,7 @@ public abstract class GenericCallCapableJobHandler<P,C> extends GenericJobHandle
         CompletableFuture<Void> ft;
         boolean starting=true;
         Object context;
+        Object retval;
     }
     Map<Long,JobExecutionState> queuesMap = new ConcurrentHashMap<>();
     ExecutorService es = Executors.newFixedThreadPool(16);
@@ -45,49 +47,35 @@ public abstract class GenericCallCapableJobHandler<P,C> extends GenericJobHandle
     }
 
     public class CallCapableJob extends Job {
-        public <PJh, CJh, Jh extends GenericJobHandler<PJh, CJh>, T> T call(Jh jobHandler, PJh p, String message, Class<T> clazz) {
+
+        public <PJh, CJh, Jh extends GenericJobHandler<PJh, CJh>, T> T call(Jh jobHandler, PJh p, String message, Class<T> clazz) throws InterruptedException {
             var qv = new FromWorker();
             qv.type = QElementType.CALL;
             qv.message = message;
             var jes = queuesMap.get(getId());
-            try {
-                var jen = startAndWait(jobHandler, p);
-                jes.qFromWorker.put(qv);
-                jes.qToWorker.take();
-                return jen.getReturnValue(clazz);
-            } catch (InterruptedException e) {
-                log.error("Exception", e);
-                throw new RuntimeException(e);
-            }
+            var jen = startAndWait(jobHandler, p);
+            jes.qFromWorker.put(qv);
+            jes.qToWorker.take();
+            return jen.getReturnValue(clazz);
         }
 
-        public void join(String message){
+        public void commitAndJoin(String message) throws InterruptedException {
             var qv = new FromWorker();
             qv.type = QElementType.STORE;
             qv.message = message;
             var jes = queuesMap.get(getId());
-            try {
-                jes.qFromWorker.put(qv);
-                jes.qToWorker.take();
-            } catch (InterruptedException e) {
-                log.error("Exception", e);
-                throw new RuntimeException(e);
-            }
+            jes.qFromWorker.put(qv);
+            jes.qToWorker.take();
         }
 
-        public void sleep(Duration duration, String message) {
+        public void sleep(Duration duration, String message) throws InterruptedException {
             var qv = new FromWorker();
             qv.type = QElementType.SLEEP;
             qv.message = message;
             qv.sleepDuration = duration;
             var jes = queuesMap.get(getId());
-            try {
-                jes.qFromWorker.put(qv);
-                jes.qToWorker.take();
-            } catch (InterruptedException e) {
-                log.error("Exception", e);
-                throw new RuntimeException(e);
-            }
+            jes.qFromWorker.put(qv);
+            jes.qToWorker.take();
         }
 
         public CallCapableJob(Job j) {
@@ -111,26 +99,33 @@ public abstract class GenericCallCapableJobHandler<P,C> extends GenericJobHandle
     @Override
     public JobState execute(Job job, P p, C c) throws Exception {
         Long jobId = job.getId();
-        var jes = queuesMap.get(jobId);
+        final var jes = queuesMap.get(jobId);
         if(jes==null){ //first job run
-            jes = new JobExecutionState();
-            jes.qFromWorker = new SynchronousQueue<>(true);
-            jes.qToWorker = new SynchronousQueue<>(true);
-            queuesMap.put(jobId, jes);
+            var njes = new JobExecutionState();
+            njes.qFromWorker = new SynchronousQueue<>(true);
+            njes.qToWorker = new SynchronousQueue<>(true);
+            queuesMap.put(jobId, njes);
             return JobState.CONTINUE("Started");
         }else{
             if(jes.starting) {
-                JobExecutionState finalJes = jes;
-                finalJes.starting = false;
-                finalJes.context = c;
-                finalJes.ft = CompletableFuture.supplyAsync(()->{
+                jes.starting = false;
+                jes.context = c;
+                jes.ft = CompletableFuture.supplyAsync(()->{
                     try {
-                        finalJes.qFromWorker.put(retval(realExecute(new CallCapableJob(job), p, c)));
+                        jes.qFromWorker.put(retval(realExecute(new CallCapableJob(job), p, c)));
                         return null;
-                    } catch (Exception e) {
+                    } catch (InterruptedException e) {
+                        return null;
+                    } catch (Throwable e) {
                         log.error("Exception", e);
-                        throw new RuntimeException(e);
+                        try {
+                            jes.qFromWorker.put(retval(JobState.STOP(e.getMessage())));
+                        } catch (InterruptedException ex) {
+                            log.error("Got InterruptedException during exceptional return from job handler");
+                            throw new RuntimeException(ex);
+                        }
                     }
+                    return null;
                 }, es);
             }else{
                 jes.qToWorker.put(toWorker);
@@ -138,16 +133,24 @@ public abstract class GenericCallCapableJobHandler<P,C> extends GenericJobHandle
         }
 
         var fw = jes.qFromWorker.take();
+        log.trace("fw:{}", fw);
         if(fw.type==QElementType.CALL) {
             return JobState.CONTINUE(fw.message);
         } else if (fw.type==QElementType.STORE) {
             log.info("STORE:{}", fw.message);
-            synchronized (this) {
+            synchronized (jes) {
                 BeanUtils.copyProperties(jes.context, c);
-                return JobState.CONTINUE(fw.message);
             }
+            return JobState.CONTINUE(fw.message);
         }else if(fw.type==QElementType.RETURN){
-            return fw.jobState;
+            synchronized (jes) {
+                BeanUtils.copyProperties(jes.context, c);
+            }
+            String notes = fw.jobState.getMessage();
+            if(!List.of(JobState.Status.DONE, JobState.Status.STOP, JobState.Status.ABORT).contains(fw.jobState.getStatus())){
+                log.error("(*** realExecute returns with unexpected status!="+ fw.jobState.getStatus()+"***)"+notes);
+            }
+            return JobState.DONE(fw.jobState.getReturnValue(), notes);
         }else if(fw.type == QElementType.SLEEP){
             log.info("SLEEP:{} for {}", fw.message, fw.sleepDuration);
             return JobState.CONTINUE(fw.message, fw.sleepDuration);
